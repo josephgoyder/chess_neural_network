9df29a71bdc2e9ed881fb89b7ead071f8b42ef2c
josephchiao <chiaofeng.0118@gmail.com>
2022-04-21 10:49:07 -0700

    complete basic NN

--- nn_chess_engine/feedforward_prop.m
+++ nn_chess_engine/feedforward_prop.m
@@ -1,1 +1,23 @@
-#test 5
+function p = predict(Theta1, Theta2, can_p, X)
+
+m = size(X, 1);
+
+p = zeros(size(X, 1), 1);
+
+X = [ones(m, 1) X];
+
+
+a_2 = sigmoid(X * Theta1');
+
+a_2 = [ones(size(a_2), 1) a_2];
+
+for i = can_p,
+  if can_p = 1:
+    p = sigmoid(X() * Theta2'());
+  end
+
+[z p] = max(p, [], 2);   
+% =========================================================================
+
+
+end
