1c6d1bcb090232e093171577aca87addc0cb812a
Joseph <joseph.goyder@gmail.com>
2022-05-02 11:07:41 -0700

    started on fight

--- fight.py
+++ fight.py
@@ -1,13 +1,16 @@
 import move_undo as mv
+import engine as eg
+import game as gm
 import numpy as np
 import oct2py
 from pieces import King 
+import random
 
 
 def board_to_X(board):
     X = []
     for piece in board.white_pieces + board.black_pieces:
-        X += [piece.location[0], piece.location[1], int(piece.captured)]
+        X += [piece.location[0] / 8, piece.location[1] / 8, int(piece.captured)]
 
     return np.array(X)
 
@@ -23,3 +26,17 @@ def piece_and_difference(option):
 
 def branches_to_can_p(branches):
     return np.array([option is dict for option in branches])
+
+
+def fight(theta1, theta2):
+    engine = eg.engine_setup("regular")
+    win_lose_draw = 0
+
+    while True:
+        win_lose_draw = engine.win_lose_draw()
+        if abs(win_lose_draw) == 1000:
+            win_lose_draw = engine.mate_eval()
+
+        
+
+    if 
