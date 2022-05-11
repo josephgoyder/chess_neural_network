88daa69fb23bfe6b65b08a779a3ce5d3905fd64f
josephchiao <chiaofeng.0118@gmail.com>
2022-05-11 11:38:01 -0700

    ditch can_p

--- feedforward_prop.m
+++ feedforward_prop.m
@@ -1,7 +1,7 @@
-function [p, time] = feedforward_prop(can_p, X, Theta)
+function [p, time] = feedforward_prop(X, Theta)
 
 tic;
-load(["C:\\Users\\076-jgoyder\\Chess engine\\chess_neural_network\\engine_data\\neural_net_dataset_" num2str(Theta) ".mat"])
+load(["/Users/joseph_chiao/Desktop/Advance Research/Machine Learning/Chess engine/chess_neural_network/engine_data/neural_net_dataset_" num2str(Theta) ".mat"])
 
 m = size(X, 1);
 
@@ -17,10 +17,11 @@ a_3 = sigmoid(a_2 * Theta2);
 
 a_3 = [ones(size(a_3), 1) a_3];
 
-p = feedforward_prop_config_o_layer(p, Theta3, can_p, a_3);
+p = sigmoid(a_3 * Theta3);
 
 [z p] = max(p, [], 2);   
 
 
 time = toc;
+
 end
