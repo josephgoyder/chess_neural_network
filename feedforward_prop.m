0d9a6505b4138b1057fcb6c9406a58365879d8c7
josephchiao <chiaofeng.0118@gmail.com>
2022-06-13 10:02:15 -0700

    change output mode

--- feedforward_prop.m
+++ feedforward_prop.m
@@ -1,4 +1,4 @@
-function [p, time] = feedforward_prop(can_p, X, Theta)
+function [p, time] = feedforward_prop(X, Theta)
 
 tic;
 % load(["C:\\Users\\076-jgoyder\\Chess engine\\chess_neural_network\\engine_data\\neural_net_dataset_" num2str(Theta) ".mat"])
@@ -19,30 +19,8 @@ a_3 = sigmoid(a_2 * Theta2);
 
 a_3 = [ones(size(a_3), 1) a_3];
 
-p = feedforward_prop_config_o_layer(p, Theta3, can_p, a_3);
-
-maxes_i = zeros(1, 2);
-maxes = zeros(1, 2);
-for i = 1:length(p),
-    for k = 1:2,
-        if p(i) > maxes(k),
-            maxes_i(k) = i;
-            maxes(k) = p(i);
-            break;
-        endif
-    endfor
-end
-
-choice_ratio = maxes(2) / (maxes(1) + maxes(2));
-choice = rand;
-
-if choice > choice_ratio,
-    p = maxes_i(1);
-endif
-
-if choice < choice_ratio,
-    p = maxes_i(2);
-endif
+p = sigmoid(a_3 * Theta3);
 
 time = toc;
+
 end
