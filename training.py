7bc2d60210690a589d34a5a2174d81381a392c6f
Joseph <joseph.goyder@gmail.com>
2022-06-13 10:03:46 -0700

    sets dataset 2 to dataset 1 after backprop

--- training.py
+++ training.py
@@ -1,13 +1,15 @@
 import fight as ft
 from oct2py import octave
+import shutil
 
 
 #$env:path += ";C:\Users\076-jgoyder\AppData\Local\Programs\GNU Octave\Octave-7.1.0\mingw64\bin"
 octave = octave.Oct2Py()
 for x in range(10):
-    octave.mutate(2, 0.1)
+    octave.mutate(2, 1)
     X, y = ft.fight()
     octave.back_prop(1, X, y, 1)
-    # set dataset 2 to what it was before
+    shutil.copyfile('/home/joseph/Desktop/chess_neural_network/engine_data/neural_net_dataset_1.mat', '/home/joseph/Desktop/chess_neural_network/engine_data/neural_net_dataset_2.mat')
+    # set dataset 2 to dataset 1
 
 
