87e57ab26f97fbe2327389b9f004ad805c2fbcf9
Joseph <joseph.goyder@gmail.com>
2022-06-15 12:21:19 -0700

    made different functions for training with and without old engine

--- fight.py
+++ fight.py
@@ -5,7 +5,7 @@ import eval as ev
 import numpy as np
 
             
-def fight():
+def train_assisted():
     game = gm.game_start_nn()
     engine_comb = Engine_regular(
         game.engine.history, 
@@ -59,3 +59,37 @@ def fight():
 
     return np.array(X_nn + X_comb), np.array(y_nn + y_comb)
 
+
+def train():
+    game = gm.game_start_nn()
+
+    game.engine.depth = 2
+
+    X_nn = []
+    move_n = 0
+
+    while not game.concluded():
+        game.engine_turn()
+        X_nn.append(ev.board_to_X(game.engine.board, game.turn))
+        move_n += 1
+        if move_n == 2:
+            game.draw = True
+
+        print("Move: ", move_n)
+
+    print("")
+    if game.white_win:
+        print("White wins")
+        eval = 1
+
+    elif game.black_win:
+        print("Black wins")
+        eval = 0
+
+    elif game.draw:
+        print("Draw")
+        eval = 0.5
+
+    y_nn = [eval] * move_n 
+
+    return np.array(X_nn), np.array(y_nn)
