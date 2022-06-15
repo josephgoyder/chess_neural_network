9f23861b2683973edce2d5d8c85e0821664e9984
Joseph <joseph.goyder@gmail.com>
2022-06-15 11:32:26 -0700

    added training on old engine

--- fight.py
+++ fight.py
@@ -1,3 +1,4 @@
+from engine import Engine_regular, engine_setup
 import game as gm
 import pieces as pc
 import eval as ev
@@ -6,16 +7,37 @@ import numpy as np
             
 def fight():
     game = gm.game_start_nn()
-    X = []
+    engine_comb = Engine_regular(
+        game.engine.history, 
+        game.engine.notebook, 
+        game.engine.board, 
+        4,
+        [1, 1],
+        10.0, 
+        1.0
+    )
+
+    X_nn = []
+    X_comb = []
+    y_comb = []
     move_n = 0
 
     while not game.concluded():
         game.engine_turn()
-        X.append(ev.board_to_X(game.engine.board, game.turn))
+        X_nn.append(ev.board_to_X(game.engine.board, game.turn))
+
+        engine_comb.explore(game.turn)
+        move_comb = engine_comb.notebook.top_lines[0][1][0]
+        for branch in game.engine.branches():
+            game.engine.move(branch, game.turn)
+            X_comb.append(ev.board_to_X(game.engine.board, game.turn))
+            y_comb.append(branch == move_comb)
+            game.engine.undo()
+
         move_n += 1
         if move_n == 200:
             game.draw = True
-            
+
         print("Move: ", move_n)
         print("X size:", np.shape(X))
 
@@ -32,7 +54,7 @@ def fight():
         print("Draw")
         eval = 0.5
 
-    y = [eval] * move_n 
+    y_nn = [eval] * move_n 
 
     return np.array(X), np.array(y)
 
