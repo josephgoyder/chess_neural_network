8ed36f984dd219a95d910166892162de5c8f39f5
Joseph <joseph.goyder@gmail.com>
2022-06-16 11:22:22 -0700

    small fix

--- back_prop.m
+++ back_prop.m
@@ -37,4 +37,4 @@ function[J] = back_prop(Theta, X, y, lambda)
   Theta3 = Theta3';
 
   # save(["/home/joseph/Desktop/chess_neural_network/engine_data/neural_net_dataset_" num2str(Theta) ".mat"], "Theta1", "Theta2", "Theta3")
-  load(["C:\\Users\\076-jgoyder\\Chess engine\\chess_neural_network\\engine_data\\neural_net_dataset_" num2str(Theta) ".mat"], "Theta1", "Theta2", "Theta3")
+  save(["C:\\Users\\076-jgoyder\\Chess engine\\chess_neural_network\\engine_data\\neural_net_dataset_" num2str(Theta) ".mat"], "Theta1", "Theta2", "Theta3")
