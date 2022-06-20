7819457b0c6abee0fe5481032e5010196ae821cb
josephchiao <chiaofeng.0118@gmail.com>
2022-06-20 10:10:14 -0700

    fixed some big time fuck up

--- back_prop.m
+++ back_prop.m
@@ -5,7 +5,6 @@ function[J] = back_prop(Theta, X, y, lambda)
 
   options = optimset('MaxIter', 50);
 
-
   input_layer_size = size(Theta1)(1) - 1;
   Theta1 = Theta1';
   hidden_layer_size = size(Theta2)(1) - 1;
@@ -17,18 +16,16 @@ function[J] = back_prop(Theta, X, y, lambda)
 
   y = y';
 
+  % Compute cost function for evaluation
   J = nnCostFunction(nn_params, input_layer_size, hidden_layer_size, hidden_layer_2_size, output_layer_size, X, y, lambda)
 
+  %Create short hand for NN_params in the cost function
   costfunction = @(p)nnCostFunction(p, input_layer_size, hidden_layer_size, hidden_layer_2_size, output_layer_size, X, y, lambda);
 
-  newTheta1 = randInitializeWeights(input_layer_size, hidden_layer_size);
-  newTheta2 = randInitializeWeights(hidden_layer_size, hidden_layer_2_size);
-  newTheta3 = randInitializeWeights(hidden_layer_2_size, output_layer_size);
+  % 
+  [nn_params, cost] = fmincg(costfunction, nn_params, options);
 
-  initial_nn_params = [newTheta1(:) ; newTheta2(:) ; newTheta3(:)];
-
-  [nn_params, cost] = fmincg(costfunction, initial_nn_params, options);
-
+  % reshaping Thetas
   Theta1 = reshape(nn_params(1:(size(Theta1)(1) * size(Theta1)(2))), size(Theta1));
   Theta1 = Theta1';
   Theta2 = reshape(nn_params(((size(Theta1)(1) * size(Theta1)(2)) + 1):(size(Theta1)(1) * size(Theta1)(2)) + (size(Theta2)(1) * size(Theta2)(2))), size(Theta2));
@@ -36,5 +33,6 @@ function[J] = back_prop(Theta, X, y, lambda)
   Theta3 = reshape(nn_params(end - (size(Theta3)(1) * size(Theta3)(2)) + 1:end), size(Theta3));
   Theta3 = Theta3';
 
+  % save to file
   # save(["/home/joseph/Desktop/chess_neural_network/engine_data/neural_net_dataset_" num2str(Theta) ".mat"], "Theta1", "Theta2", "Theta3")
   save(["C:\\Users\\076-jgoyder\\Chess engine\\chess_neural_network\\engine_data\\neural_net_dataset_" num2str(Theta) ".mat"], "Theta1", "Theta2", "Theta3")
