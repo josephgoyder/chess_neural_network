dc45fa5b9dcaf33773dd48fd945d6f5b98837891
josephchiao <chiaofeng.0118@gmail.com>
2022-06-23 10:02:33 -0700

    changed parameters and made unassissted

--- training.py
+++ training.py
@@ -9,20 +9,22 @@ import time
 
 def train(depth, games_per_step, theta):
     octave.addpath("/home/joseph/Desktop/chess_neural_network")
-    for i in range(100000):
+    for i in range(10):
         print(f"gen: {i}")
-        # n = octave.mutation(2, 100)
+        n = octave.mutation(2, 30)
         X = []
         y = []
         for j in range(games_per_step):
-            X_j, y_j = ft.train_assisted(depth)
+            X_j, y_j = ft.train_unassisted()
             X += X_j
             y += y_j
 
         n = octave.back_prop(theta, np.array(X), np.array(y), 1)
-    # shutil.copyfile('/home/joseph/Desktop/chess_neural_network/engine_data/neural_net_dataset_1.mat', '/home/joseph/Desktop/chess_neural_network/engine_data/neural_net_dataset_2.mat')
-    # shutil.copyfile('C:\\Users\\076-jgoyder\\Chess engine\\chess_neural_network\\engine_data\\neural_net_dataset_1.mat', '/home/joseph/Desktop/chess_neural_network/engine_data/neural_net_dataset_2.mat')
+        shutil.copyfile('/home/joseph/Desktop/chess_neural_network/engine_data/neural_net_dataset_1.mat', '/home/joseph/Desktop/chess_neural_network/engine_data/neural_net_dataset_2.mat')
+        # shutil.copyfile('C:\\Users\\076-jgoyder\\Chess engine\\chess_neural_network\\engine_data\\neural_net_dataset_1.mat', '/home/joseph/Desktop/chess_neural_network/engine_data/neural_net_dataset_2.mat')
 
-    # set dataset 2 to dataset 1
+        # set dataset 2 to dataset 1
+
+train(1, 1, 1)
 
 
