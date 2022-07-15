9aa12f4de87d0250406b974bda0c0421a9eceb89
josephchiao <chiaofeng.0118@gmail.com>
2022-07-15 10:52:14 -0700

    added compete to test which theta is better

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import training as tr
 import shutil
 
-tr.train(2, (250, 250, 25), 10, [1, 2], 0.01, 1)
+# tr.compete([1, 2])
+tr.train(1, (25, 25, 50), 100, [1, 2], 0.01, 1)
--- training.py
+++ training.py
@@ -13,7 +13,10 @@ def train(depth, examples, steps, thetas, _lambda, morph_rate):
     Trains the NN on the comb engine. {Depth} specifies the depth the comb engine runs on.
     '''
     octave.addpath("/home/joseph/Desktop/chess_neural_network")
-    shutil.copyfile(f'/home/joseph/Desktop/chess_neural_network/engine_data/neural_net_dataset_{thetas[0]}.mat', f'/home/joseph/Desktop/chess_neural_network/engine_data/neural_net_dataset_{thetas[1]}.mat')
+    shutil.copyfile(
+        f'/home/joseph/Desktop/chess_neural_network/engine_data/neural_net_dataset_{thetas[0]}.mat', 
+        f'/home/joseph/Desktop/chess_neural_network/engine_data/neural_net_dataset_{thetas[1]}.mat'
+    )
     for i in range(steps):
         X_win = []
         y_win = []
@@ -22,10 +25,10 @@ def train(depth, examples, steps, thetas, _lambda, morph_rate):
         X_draw = []
         y_draw = []
 
-        while len(y_win) < examples[0] or len(y_lose) < examples[1] or len(y_draw) < examples[2]:
+        while len(y_win) < examples[0] or len(y_lose) < examples[1]:
             print(f"gen: {i + 1}")
             print(f"examples (w, l, d): {(len(y_win), len(y_lose), len(y_draw))}")
-            X_j, y_j = ft.fight_unassisted([thetas[0], thetas[1]])
+            X_j, y_j = ft.fight_random_pos([thetas[0], thetas[1]])
             
             if y_j[0] == 1:
                 X_win += X_j
@@ -35,12 +38,44 @@ def train(depth, examples, steps, thetas, _lambda, morph_rate):
                 X_lose += X_j
                 y_lose += y_j
 
-            elif y_j[0] == 0.5:
+            else:
                 X_draw += X_j
                 y_draw += y_j
 
         for dataset in [X_win, X_lose, X_draw, y_win, y_lose, y_draw]:
             random.shuffle(dataset)
 
-        n = octave.back_prop(thetas[0], np.array(X_win[:examples[0]] + X_lose[:examples[1]] + X_draw[:examples[2]]), np.array(y_win[:examples[0]] + y_lose[:examples[1]] + y_draw[:examples[2]]), _lambda)
-        shutil.copyfile(f'/home/joseph/Desktop/chess_neural_network/engine_data/neural_net_dataset_{thetas[0]}.mat', f'/home/joseph/Desktop/chess_neural_network/engine_data/neural_net_dataset_{thetas[1]}.mat')
+        n = octave.back_prop(
+            thetas[0], 
+            np.array(X_win[:examples[0]] + X_lose[:examples[1]] + X_draw[:min(len(X_draw), examples[2])]), 
+            np.array(y_win[:examples[0]] + y_lose[:examples[1]] + y_draw[:min(len(y_draw), examples[2])]),
+             _lambda
+        )
+        shutil.copyfile(
+            f'/home/joseph/Desktop/chess_neural_network/engine_data/neural_net_dataset_{thetas[0]}.mat', 
+            f'/home/joseph/Desktop/chess_neural_network/engine_data/neural_net_dataset_{thetas[1]}.mat'
+        )
+
+
+def compete(thetas):
+    '''
+    Trains the NN on the comb engine. {Depth} specifies the depth the comb engine runs on.
+    '''
+    octave.addpath("/home/joseph/Desktop/chess_neural_network")
+    win = 0
+    lose = 0
+    draw = 0
+
+    while win < 20 and lose < 20:
+        X_j, y_j = ft.fight_random_pos([thetas[0], thetas[1]])
+        
+        if y_j[0] == 1:
+            win += 1
+
+        elif y_j[0] == 0:
+            lose += 1
+
+        elif y_j[0] == 0.5:
+            draw += 1
+
+    print((win, lose, draw))
