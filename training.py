e733ae362c3fa1f47e15cd32180d1012f42bad7d
josephchiao <chiaofeng.0118@gmail.com>
2022-07-20 13:41:40 -0700

    added morth and colour switch

--- training.py
+++ training.py
@@ -17,6 +17,9 @@ def train(depth, examples, steps, thetas, _lambda, morph_rate):
         f'/home/joseph/Desktop/chess_neural_network/engine_data/neural_net_dataset_{thetas[0]}.mat', 
         f'/home/joseph/Desktop/chess_neural_network/engine_data/neural_net_dataset_{thetas[1]}.mat'
     )
+
+    theta_train = thetas[0]
+    theta_morph = thetas[1]
     for i in range(steps):
         X_win = []
         y_win = []
@@ -28,7 +31,14 @@ def train(depth, examples, steps, thetas, _lambda, morph_rate):
         while len(y_win) < examples[0] or len(y_lose) < examples[1]:
             print(f"gen: {i + 1}")
             print(f"examples (w, l, d): {(len(y_win), len(y_lose), len(y_draw))}")
+
+            thetas.reverse()
+            n = octave.morph(theta_morph, morph_rate)
             X_j, y_j = ft.fight_random_pos([thetas[0], thetas[1]])
+            shutil.copyfile(
+            f'/home/joseph/Desktop/chess_neural_network/engine_data/neural_net_dataset_{theta_train}.mat', 
+            f'/home/joseph/Desktop/chess_neural_network/engine_data/neural_net_dataset_{theta_morph}.mat'
+            )
             
             if y_j[0] == 1:
                 X_win += X_j
@@ -46,14 +56,14 @@ def train(depth, examples, steps, thetas, _lambda, morph_rate):
             random.shuffle(dataset)
 
         n = octave.back_prop(
-            thetas[0], 
+            theta_train, 
             np.array(X_win[:examples[0]] + X_lose[:examples[1]] + X_draw[:min(len(X_draw), examples[2])]), 
             np.array(y_win[:examples[0]] + y_lose[:examples[1]] + y_draw[:min(len(y_draw), examples[2])]),
              _lambda
         )
         shutil.copyfile(
-            f'/home/joseph/Desktop/chess_neural_network/engine_data/neural_net_dataset_{thetas[0]}.mat', 
-            f'/home/joseph/Desktop/chess_neural_network/engine_data/neural_net_dataset_{thetas[1]}.mat'
+            f'/home/joseph/Desktop/chess_neural_network/engine_data/neural_net_dataset_{theta_train}.mat', 
+            f'/home/joseph/Desktop/chess_neural_network/engine_data/neural_net_dataset_{theta_morph}.mat'
         )
 
 
@@ -66,7 +76,7 @@ def compete(thetas):
     lose = 0
     draw = 0
 
-    while win < 20 and lose < 20:
+    while win < 100 and lose < 100:
         X_j, y_j = ft.fight_random_pos([thetas[0], thetas[1]])
         
         if y_j[0] == 1:
@@ -78,4 +88,7 @@ def compete(thetas):
         elif y_j[0] == 0.5:
             draw += 1
 
-    print((win, lose, draw))
+        print((win, lose, draw))
+
+
+    
