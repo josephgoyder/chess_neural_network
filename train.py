0da3d789c15c779481adcd077855bb0fbbbb4621
josephchiao <chiaofeng.0118@gmail.com>
2022-07-28 11:16:10 -0700

    new settings +stuff for testing against random nns

--- train.py
+++ train.py
@@ -4,16 +4,6 @@ import shutil
 import numpy as np
 import fight
 
-wins = 0
-for theta in range(2, 101):
-    winner, loser, irrelevant = fight.fight([1, theta])
-    if winner == 1:
-        wins += 1
-
-print(wins)
-
-# tr.compete([1, 2])
-# ga.genetic_algorithm(100, 5, 3, 3, 2, 3, 20, 50)
 
 # theta_size = 50
 # ga.theta_init(
@@ -22,4 +12,15 @@ print(wins)
 #     np.array([theta_size + 1, 1]), 
 #     100
 # )
+
+wins = 0
+for theta in range(47, 101):
+    winner, loser, irrelevant = fight.fight([1, theta])
+    if winner == 1:
+        wins += 1
+    
+    print(wins, theta)
+
+# tr.compete([1, 2])
+# ga.genetic_algorithm(20, 4, 100, 2, 2, 3, 20, 50)
 # tr.train(1, (1000, 1000, 2000), 5, [1, 2], 0.01, 0.3)
