3bacd39ab7d773ee1c02c1934b81219e61322a55
josephchiao <chiaofeng.0118@gmail.com>
2022-07-28 13:56:47 -0700

    changed timing to include all of function

--- morph.m
+++ morph.m
@@ -1,5 +1,5 @@
 function [n] = morph(dataset, morph_rate)
-
+    tic
     disp(["Morphing dataset ", num2str(dataset)])
 
     #Load Theta
@@ -31,12 +31,12 @@ function [n] = morph(dataset, morph_rate)
     Theta1 = reshape(new_theta_1, [size(Theta1)]);
     Theta2 = reshape(new_theta_2, [size(Theta2)]);
     Theta3 = reshape(new_theta_3, [size(Theta3)]);
-    tic
     
     % Save files
     save(["/home/joseph/Desktop/chess_neural_network/engine_data/neural_net_dataset_" num2str(dataset) ".mat"], "Theta1", "Theta2", "Theta3");
     % save(["C:\\Users\\076-jgoyder\\Chess engine\\chess_neural_network\\engine_data\\neural_net_dataset_" num2str(dataset) ".mat"], "Theta1", "Theta2", "Theta3");
-    toc
+    
     n = 0;
+    toc
     
     end
