846c8081d3b0da7618cb08b3436456ce500e1d18
Joseph Goyder <joseph.goyder@gmail.com>
2022-08-05 16:03:52 -0700

    test commit as JG

--- train.py
+++ train.py
@@ -25,6 +25,6 @@ import random
 #     print(wins, theta)
 #     p(wins, theta - wins, 0.5)
 
-tr.compete([1, 2])
-ga.genetic_algorithm(20, 20, 10, 2, 2, 20, 150, 25, self_training=True)
+# tr.compete([2, 1])
+ga.genetic_algorithm(40, 40, 10, 2, 2, 20, 150, 25, self_training=True)
 # tr.train(1, (1000, 1000, 2000), 5, [1, 2], 0.01, 0.3)
